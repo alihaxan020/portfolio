15b190a101d810c780e753f6c296867ab14b710d
alihassan020 <alihassan4651@gmail.com>
2021-11-18 08:46:58 +0500

    improvements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Intro from "./components/introduction/Intro";
 import About from "./components/about/About";
+import ProductList from "./components/productList/ProductList";
 
 function App() {
   return (
     <div>
       <Intro />
       <About />
+      <ProductList />
     </div>
   );
 }
